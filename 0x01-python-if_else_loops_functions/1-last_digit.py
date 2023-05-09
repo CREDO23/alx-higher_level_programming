143aa0061f391bed804dba4cffa5de4f4a261964
CREDO23 <thierrybakera12@gmail.com>
2023-05-10 00:37:54 +0200

    fix the last gigit checker

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,9 +2,13 @@
 import random
 number = random.randint(-10000, 10000)
 lastgd = abs(number) % 10
+
+if (number < 0):
+    lastgd = - lastgd
+
 if (lastgd > 5):
-    print(f"The last digit of {number} is {lastgd} and is greater than 5")
+    print(f"Last digit of {number} is {lastgd} and is greater than 5")
 elif (lastgd < 6 and lastgd != 0):
-    print(f"The last digit of {number} is {lastgd} and is less than 6 and not 0")
+    print(f"Last digit of {number} is {lastgd} and is less than 6 and not 0")
 else:
-    print(f"The last digit of {number} is {lastgd} and is 0")
+    print(f"Last digit of {number} is {lastgd} and is 0")
