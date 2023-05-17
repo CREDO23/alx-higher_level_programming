36a6fab5afd78fcef886d51fd4e8a4daac7bd58a
CREDO23 <thierrybakera12@gmail.com>
2023-05-18 01:47:22 +0200

    fix best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
 
+
 def best_score(a_dictionary):
-
-    if not isinstance(a_dictionary, dict) or a_dictionary is None:
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
-    else:
 
-        best_key = ''
-        best_score = float('-inf')
-
-        for k in a_dictionary:
-            if a_dictionary[k] > best_score:
-                best_score = a_dictionary[k]
-                best_key = k
-
-        return best_key
+    best_key = list(a_dictionary)[0]
+    best_score = a_dictionary[best_key]
+    for k, v in a_dictionary.items():
+        if v > best_score:
+            best_score = v
+            best_key = k
+    return (best_key)
