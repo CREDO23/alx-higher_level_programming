83488e8f603054348305db259427d1d0c9dcdb38
CREDO23 <thierrybakera12@gmail.com>
2023-05-18 01:34:10 +0200

    fix find best score method

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,11 +4,13 @@ def best_score(a_dictionary):
 
     if a_dictionary is None:
         return None
-
-    best_key = list(a_dictionary)[0]
+    
+    best_key = ''
+    best_score = float('-inf')
 
     for k in a_dictionary:
-        if a_dictionary[k] > a_dictionary[best_key]:
+        if a_dictionary[k] > best_score:
+            best_score = a_dictionary[k]
             best_key = k
 
     return best_key
