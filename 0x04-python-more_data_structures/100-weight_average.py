07370787ebd7cada5d0cf0b16f5174bc35468303
CREDO23 <thierrybakera12@gmail.com>
2023-05-18 03:04:59 +0200

    add the weight avrage function

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def weight_average(my_list=[]):
+    score = 0
+    weight_sum = 0
+
+    for i in my_list:
+        score += i[0] * i[1]
+        weight_sum += i[1]
+
+    return score / weight_sum
