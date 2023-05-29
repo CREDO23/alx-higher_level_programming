8d1d137711a3989ecea33b3c5ee259bd3d327c70
CREDO23 <thierrybakera12@gmail.com>
2023-05-29 23:38:55 +0200

    safe function

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from sys import stderr
+
+
+def safe_function(fct, *args):
+    result = 0
+    try:
+        result = fct(*args)
+    except Exception as e:
+        stderr.write("Exception: {}\n".format(e))
+        result = None
+    return result
