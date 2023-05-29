acc86a92ae9557eba72cf365625a7563c90b206a
CREDO23 <thierrybakera12@gmail.com>
2023-05-29 23:26:51 +0200

    safe print a integer with writting exception message in stderr

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from sys import stderr
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+
+        return True
+    except Exception as te:
+        stderr.write("{}\n".format(te))
+        return False
