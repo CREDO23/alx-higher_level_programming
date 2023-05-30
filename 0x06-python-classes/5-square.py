b31495694edfb4869b4b2afe368890b5b5f2530a
CREDO23 <thierrybakera12@gmail.com>
2023-05-31 00:54:24 +0200

    write in the stdout the area ith a character

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+"""Implementation of a Square class"""
+
+
+class Square():
+    """Defines a square"""
+    def __init__(self, size=0):
+
+        """Initializing this square class
+        Raises:
+            TypeError: if size is not integer
+            ValueError: if size is less than zero
+        """
+
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
+
+    def area(self):
+        """Returns the area of the square"""
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """Returns the size of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, newsize):
+        """Set the size of the square"""
+        if not isinstance(newsize, int):
+            raise TypeError('size must be an integer')
+        if newsize < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = newsize
+
+    def my_print(self):
+
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end='')
+                print("")
