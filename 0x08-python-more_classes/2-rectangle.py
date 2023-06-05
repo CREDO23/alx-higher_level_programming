f54d070e98a15959476dbe7d455aec2ed035bd17
CREDO23 <thierrybakera12@gmail.com>
2023-06-06 00:48:27 +0200

    add area and perimeter method

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+'''Rectangle class'''
+
+
+class Rectangle:
+    '''An empty class Rectangle'''
+    def __init__(self, width=0, height=0):
+        '''Initializes a new Rectangle'''
+        self.width = width
+        self.height = height
+
+    @property
+    def height(self):
+        '''get the height of the rectangle'''
+        return self.__height
+
+    @height.setter
+    def height(self, h):
+        '''Set the height of the rectangle'''
+        if not isinstance(h, int):
+            raise TypeError('height must be an integer')
+        if h < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = h
+
+    @property
+    def width(self):
+        '''Get the width of the rectangle'''
+        return self.__width
+
+    @width.setter
+    def width(self, w):
+        '''Set the width of the rectangle'''
+        if not isinstance(w, int):
+            raise TypeError('width must be an integer')
+        if w < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = w
+
+    def area(self):
+        '''Return the area of the rectangle'''
+        return self.__width * self.__height
+
+    def perimeter(self):
+        '''Return the perimeter of the rectangle'''
+        return (self.__width + self.__height) * 2
