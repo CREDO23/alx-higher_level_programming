9a0455ca207b4c785d7d43b37d4773b99d8ce548
CREDO23 <thierrybakera12@gmail.com>
2023-06-06 22:38:16 +0200

    add docstring to the add_integer function

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
+"""
+
+This module has one function that adds up 2 integers
+
+"""
+
 
 def add_integer(a, b=98):
-    if isinstance(a, float):
-        a = int(a)
-    if isinstance(b, float):
-        b = int(b)
-
-    if not isinstance(a, int):
-        raise TypeError('a must be an integer')
-    if not isinstance(b, int):
-        raise TypeError('b must be an integer')
-
-    return a + b
+    """
+    Return the sum of two integers or floats as integers
+    """
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
