30eddacdd34666d9fe99ffa83e651803e2bb938e
CREDO23 <thierrybakera12@gmail.com>
2023-06-12 20:56:40 +0200

    add doctstring to the function

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+'''This module contains a function \
+      that looks up for attributes and methods inside a class'''
+
 
 def lookup(obj):
+    '''Returns a list of attributes and methods of the given object(obj)'''
     return dir(obj)
