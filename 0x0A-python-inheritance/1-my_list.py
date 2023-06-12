ad73fcec2a2cfc00c27d4658923ae3419dec87d8
CREDO23 <thierrybakera12@gmail.com>
2023-06-12 22:14:15 +0200

    add test cases for MyList class

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,10 +6,6 @@
 class MyList(list):
     '''This class implements inherit the list class'''
 
-    def __init__(self):
-        '''Intialize the class'''
-        super().__init__(self)
-
     def print_sorted(self):
         '''Print the list in sorted order'''
         print(sorted(self))
