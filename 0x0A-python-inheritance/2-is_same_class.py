a1d8942c2dc6871c1922013ce255bc097d6de0cd
CREDO23 <thierrybakera12@gmail.com>
2023-06-12 22:31:13 +0200

    check for class

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+'''This module contains a\
+      function which chech if an object is an exact match of a class'''
+
 
 def is_same_class(obj, a_class):
+    '''Return true if obj is an\
+          exact match of a class a_class otherwise False'''
     return type(obj) == a_class
