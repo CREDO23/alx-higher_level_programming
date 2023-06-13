803b327549a527108c40e7b37d4debc423836777
CREDO23 <thierrybakera12@gmail.com>
2023-06-13 23:09:46 +0200

    read a file with "with" statement

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''This module contains a function\
+      that reads a text file (UTF8) and prints it to stdout'''
+
+
+def read_file(filename=""):
+    '''Reads a text file and print it to stdout'''
+
+    with open(filename, encoding='UTF8') as f:
+        file = f.read()
+        print(file)
