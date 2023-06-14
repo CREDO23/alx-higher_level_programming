74a547c752d28f2d58877b77804b43d1435152ab
CREDO23 <thierrybakera12@gmail.com>
2023-06-14 03:28:49 +0200

    improve the pascal triangle algo

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -12,7 +12,7 @@ def pascal_triangle(n):
 
     arr.append([1])
 
-    for i in range(1, n + 1):
+    for i in range(1, n):
         row = []
         row.append(1)
         for j in range(2, i+1):
@@ -22,4 +22,5 @@ def pascal_triangle(n):
                 row.append(arr[i-1][j-1] + arr[i-1][j-2])
         row.append(1)
         arr.append(row)
+
     return arr
