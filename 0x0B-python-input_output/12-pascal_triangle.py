838aef4728e467779c1f67384a40c8805bde5065
CREDO23 <thierrybakera12@gmail.com>
2023-06-14 03:18:59 +0200

    pascal triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''This module contains the pascal triangle implementation'''
+
+
+def pascal_triangle(n):
+    '''This function implements the pascal triangulation'''
+
+    arr = []
+
+    if n <= 0:
+        return []
+
+    arr.append([1])
+
+    for i in range(1, n + 1):
+        row = []
+        row.append(1)
+        for j in range(2, i+1):
+            if (i <= 2):
+                row.append(j)
+            else:
+                row.append(arr[i-1][j-1] + arr[i-1][j-2])
+        row.append(1)
+        arr.append(row)
+    return arr
